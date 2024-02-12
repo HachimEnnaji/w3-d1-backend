5266249fef8ae863120d8362f7d8ce9623864325
HachimEnnaji <ashennaji@hotmail.it>
2024-02-12 15:24:57 +0100

    Aggiungere i file di progetto.

---
+++ FirstProject/FirstProject/Default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FirstProject._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+    <asp:Button id="bottone" runat="server" CssClass="btn btn-primary" text="Cliccami" OnClick="bottone_Click"/>
+        <h2 id="demo" CssClass="display-1 p-2" runat="server"> </h2>
+    </main>
+
+</asp:Content>
---
+++ FirstProject/FirstProject/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+using System.Web.UI;
+
+namespace FirstProject
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void bottone_Click(object sender, EventArgs e)
+        {
+            demo.InnerHtml = ConfigurationManager.AppSettings["Nome"] + " ";
+            demo.InnerHtml += ConfigurationManager.AppSettings["Cognome"];
+        }
+    }
+}
